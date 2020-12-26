72e3781b10b66caa004f263735bf229bbe7274c5
Tolga Nogay <47275502+TolgaNogay@users.noreply.github.com>
2020-12-26 21:06:57 +0300

    👋 - The file has been successfully added.
    The files were completely uploaded to the internet.

---
+++ 24.12.2020_mimarlik/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace _24._12._2020_mimarlik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "VillaDaire",
+            "VillaDaire2"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(152, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(11, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(261, 246);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 382);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 24.12.2020_mimarlik/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _24._12._2020_mimarlik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         
+           _                                                    
+ | |      | |                                            
+ | |_ ___ | | __ _  __ _   _ __   ___   __ _  __ _ _   _ 
+ | __/ _ \| |/ _` |/ _` | | '_ \ / _ \ / _` |/ _` | | | |
+ | || (_) | | (_| | (_| | | | | | (_) | (_| | (_| | |_| |
+  \__\___/|_|\__, |\__,_| |_| |_|\___/ \__, |\__,_|\__, |
+              __/ |                     __/ |       __/ |
+             |___/                     |___/       |___/ 
+
+        𝐭𝐨𝐥𝐠𝐚𝐧𝐨𝐠𝐚𝐲.𝐜𝐨𝐦 / 𝐓𝐨𝐥𝐠𝐚 N𝐨𝐠𝐚𝐲
+
+         */
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex==0)
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("Perspektif");
+                comboBox2.Items.Add("Zemin Kat");
+                comboBox2.Items.Add("Birinci Kat");
+            }
+            else if (comboBox1.SelectedIndex==1)
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("Perspektif B");
+                comboBox2.Items.Add("Zemin Kat B");
+                comboBox2.Items.Add("Birinci Kat B");
+                    
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem == "Perspektif")
+            {
+                pictureBox1.Image = Image.FromFile("D:\\TolgaNogay/Okul Örnekleri/24.12.2020_mimarlik/villa plan/2katlivilla2/1.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else if (comboBox2.SelectedItem == "Zemin")
+            {
+                pictureBox1.Image = Image.FromFile("D:\\TolgaNogay/Okul Örnekleri/24.12.2020_mimarlik/villa plan/2katlivilla2/2.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else if (comboBox2.SelectedItem == "Birinci Kat")
+            {
+                pictureBox1.Image = Image.FromFile("D:\\TolgaNogay/Okul Örnekleri/24.12.2020_mimarlik/villa plan/2katlivilla2/3.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else if (comboBox2.SelectedItem == "Perspektif B")
+            {
+                pictureBox1.Image = Image.FromFile("D:\\TolgaNogay/Okul Örnekleri/24.12.2020_mimarlik/villa plan/2katlivilla/1.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else if (comboBox2.SelectedItem == "Zemin Kat B")
+            {
+                pictureBox1.Image = Image.FromFile("D:\\TolgaNogay/Okul Örnekleri/24.12.2020_mimarlik/villa plan/2katlivilla/2.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else if (comboBox2.SelectedItem == "Birinci Kat B")
+            {
+                pictureBox1.Image = Image.FromFile("D:\\TolgaNogay/Okul Örnekleri/24.12.2020_mimarlik/villa plan/2katlivilla/1.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                MessageBox.Show("Seçim yapılmadı.");
+            }
+        }
+    }
+}
